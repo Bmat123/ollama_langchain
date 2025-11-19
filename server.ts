4769968fd2ef9d50aa162cacf80786dd820a7d2a
OS-bartmatejczyk <bart.matejczyk@outsystems.com>
2025-11-19 19:57:04 +0100

    Added training activity classes and testing

--- server.ts
+++ server.ts
@@ -1,18 +1,16 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import path from 'path'; // Import the 'path' module
-import { generateJsonData } from './data-generator';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import path from "path"; // Import the 'path' module
+import { generateJsonData } from "./data-generator";
 
 const app = express();
 const port = 3000;
 
 app.use(cors()); // Enable CORS for local development
 
-// Serve static files from the 'public' directory
-// This path works for both 'npm run dev' and 'npm start' from the dist folder
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get('/data', (req: Request, res: Response) => {
+app.get("/data", (req: Request, res: Response) => {
   const data = generateJsonData();
   res.json(data);
 });
